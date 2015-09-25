db85187e5dd8761a9d6303fedee24709e9ac4f09
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-25 17:42:50 +0700

    switch to AVA for testing
    https://github.com/sindresorhus/ava

--- test.js
+++ test.js
@@ -1,30 +1,33 @@
-/* global it */
-'use strict';
-var assert = require('assert');
-var fnName = require('./');
+/* eslint-disable prefer-arrow-callback */
+import test from 'ava';
+import fn from './';
 
-it('should return the name of named functions', function () {
-	assert.equal(fnName(function foo() {}), 'foo');
+test('named functions', t => {
+	t.is(fn(function foo() {}), 'foo');
+	t.end();
 });
 
-it('anonymous', function () {
-	assert.equal(fnName(function() {}), null); // eslint-disable-line
-	assert.equal(fnName(function () {}), null);
+test('anonymous', t => {
+	t.is(fn(function() {}), null); // eslint-disable-line
+	t.is(fn(function () {}), null);
+	t.end();
 });
 
-it('nested', function () {
-	assert.equal(fnName(function () {
+test('nested', t => {
+	t.is(fn(function () {
 		function nested() {}
 		nested();
 	}), null);
+	t.end();
 });
 
-it('nested callback', function () {
+test('nested callback', t => {
 	function call(fn) {
 		fn();
 	}
 
-	assert.equal(fnName(function () {
+	t.is(fn(function () {
 		call(function callback() {});
 	}), null);
+	t.end();
 });
