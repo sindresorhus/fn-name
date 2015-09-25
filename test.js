8f37dcd99c264700bac3685df76b4cfb031b7b41
Kazato Sugimoto <uiureo@gmail.com>
2015-09-25 17:34:39 +0900

    Setup xo

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/* global it */
 'use strict';
 var assert = require('assert');
 var fnName = require('./');
@@ -7,20 +8,25 @@ it('should return the name of named functions', function () {
 });
 
 it('anonymous', function () {
+	/* eslint-disable */
 	assert.equal(fnName(function() {}), null);
+	/* eslint-enable */
 	assert.equal(fnName(function () {}), null);
 });
 
 it('nested', function () {
 	assert.equal(fnName(function () {
 		function nested() {}
+		nested();
 	}), null);
 });
 
 it('nested callback', function () {
-	function call (fn) { fn() }
+	function call(fn) {
+		fn();
+	}
 
 	assert.equal(fnName(function () {
-		call(function callback () {});
+		call(function callback() {});
 	}), null);
 });
